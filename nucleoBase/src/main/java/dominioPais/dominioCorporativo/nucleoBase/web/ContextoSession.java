61c0c36c88fd6f2698785d775756e5dfa41274e6
Yosbany Tejas <Yosbany Tejas@DESKTOP-T4INID1>
2015-09-08 00:58:07 -0300

    dsf

--- nucleoBase/src/main/java/dominioPais/dominioCorporativo/nucleoBase/web/ContextoSession.java
+++ nucleoBase/src/main/java/dominioPais/dominioCorporativo/nucleoBase/web/ContextoSession.java
@@ -17,4 +17,52 @@ public class ContextoSession implements IContextoSession, Serializable {
 
 	private static final long serialVersionUID = 1334287978743760751L;
 
+	@Override
+	public IContextoSession navigateTo(String viewId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IContextoSession navigateToHomePage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IContextoSession navigateToPreviousPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPreviousPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAtHomePage() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getCurrentViewId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getViewIdByKey(String menuKey) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <F> F getLoggedUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- nucleoBase/src/main/java/dominioPais/dominioCorporativo/nucleoBase/web/ControladorBase.java
+++ nucleoBase/src/main/java/dominioPais/dominioCorporativo/nucleoBase/web/ControladorBase.java
@@ -14,7 +14,7 @@ public class ControladorBase implements Serializable {
 	private static final long serialVersionUID = 2524446270951076886L;
 
 	@Autowired
-	private IContextoSession session;
+	protected IContextoSession session;
 
 	private Map<String, Object> attributes;	
 
